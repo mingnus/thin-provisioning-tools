6e21196e5ae0a0f64a9642345e5e41ab34e8d096
Ming-Hung Tsai <mtsai@redhat.com>
2021-09-16 17:09:47 +0800

    [era_dump (rust)] First code drop

---
+++ src/bin/era_dump.rs
@@ -0,0 +1,77 @@
+extern crate clap;
+extern crate thinp;
+
+use clap::{App, Arg};
+use std::path::Path;
+use std::process;
+use thinp::era::dump::{dump, EraDumpOptions};
+use thinp::file_utils;
+
+//------------------------------------------
+
+fn main() {
+    let parser = App::new("era_dump")
+        .version(thinp::version::tools_version())
+        .about("Dump the era metadata to stdout in XML format")
+        // flags
+        .arg(
+            Arg::with_name("ASYNC_IO")
+                .help("Force use of io_uring for synchronous io")
+                .long("async-io")
+                .hidden(true),
+        )
+        .arg(
+            Arg::with_name("LOGICAL")
+                .help("Fold any unprocessed write sets into the final era array")
+                .long("logical"),
+        )
+        .arg(
+            Arg::with_name("REPAIR")
+                .help("Repair the metadata whilst dumping it")
+                .short("r")
+                .long("repair"),
+        )
+        // options
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Specify the output file rather than stdout")
+                .short("o")
+                .long("output")
+                .value_name("FILE"),
+        )
+        // arguments
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Specify the input device to dump")
+                .required(true)
+                .index(1),
+        );
+
+    let matches = parser.get_matches();
+    let input_file = Path::new(matches.value_of("INPUT").unwrap());
+    let output_file = if matches.is_present("OUTPUT") {
+        Some(Path::new(matches.value_of("OUTPUT").unwrap()))
+    } else {
+        None
+    };
+
+    if let Err(e) = file_utils::is_file_or_blk(input_file) {
+        eprintln!("Invalid input file '{}': {}.", input_file.display(), e);
+        process::exit(1);
+    }
+
+    let opts = EraDumpOptions {
+        input: input_file,
+        output: output_file,
+        async_io: matches.is_present("ASYNC_IO"),
+        logical: matches.is_present("LOGICAL"),
+        repair: matches.is_present("REPAIR"),
+    };
+
+    if let Err(reason) = dump(opts) {
+        eprintln!("{}", reason);
+        process::exit(1);
+    }
+}
+
+//------------------------------------------
