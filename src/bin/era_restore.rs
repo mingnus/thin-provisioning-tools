291cb3487eafcf84ad6d270c2d99bee9d724defe
Ming-Hung Tsai <mtsai@redhat.com>
2021-09-17 17:43:15 +0800

    [era_restore (rust)] First code drop

---
+++ src/bin/era_restore.rs
@@ -0,0 +1,83 @@
+extern crate clap;
+extern crate thinp;
+
+use atty::Stream;
+use clap::{App, Arg};
+use std::path::Path;
+use std::process;
+use std::sync::Arc;
+use thinp::era::restore::{restore, EraRestoreOptions};
+use thinp::file_utils;
+use thinp::report::*;
+
+fn main() {
+    let parser = App::new("era_restore")
+        .version(thinp::version::tools_version())
+        .about("Convert XML format metadata to binary.")
+        // flags
+        .arg(
+            Arg::with_name("ASYNC_IO")
+                .help("Force use of io_uring for synchronous io")
+                .long("async-io")
+                .hidden(true),
+        )
+        .arg(
+            Arg::with_name("QUIET")
+                .help("Suppress output messages, return only exit code.")
+                .short("q")
+                .long("quiet"),
+        )
+        // options
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Specify the input xml")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Specify the output device to check")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .required(true),
+        );
+
+    let matches = parser.get_matches();
+    let input_file = Path::new(matches.value_of("INPUT").unwrap());
+    let output_file = Path::new(matches.value_of("OUTPUT").unwrap());
+
+    if let Err(e) = file_utils::is_file(input_file) {
+        eprintln!("Invalid input file '{}': {}.", input_file.display(), e);
+        process::exit(1);
+    }
+
+    if let Err(e) = file_utils::check_output_file_requirements(output_file) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+
+    let report;
+
+    if matches.is_present("QUIET") {
+        report = std::sync::Arc::new(mk_quiet_report());
+    } else if atty::is(Stream::Stdout) {
+        report = std::sync::Arc::new(mk_progress_bar_report());
+    } else {
+        report = Arc::new(mk_simple_report());
+    }
+
+    let opts = EraRestoreOptions {
+        input: &input_file,
+        output: &output_file,
+        async_io: matches.is_present("ASYNC_IO"),
+        report: report.clone(),
+    };
+
+    if let Err(reason) = restore(opts) {
+        report.fatal(&format!("{}", reason));
+        process::exit(1);
+    }
+}
