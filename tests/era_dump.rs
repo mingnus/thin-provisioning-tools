9876ede5168d86757a268f3c111d0efddc19a1cc
Ming-Hung Tsai <mtsai@redhat.com>
2021-10-05 15:22:23 +0800

    [tests] Move era_dump tests to Rust

---
+++ tests/era_dump.rs
@@ -0,0 +1,109 @@
+use anyhow::Result;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+mod common;
+
+use common::common_args::*;
+use common::era::*;
+use common::fixture::*;
+use common::input_arg::*;
+use common::process::*;
+use common::program::*;
+use common::target::*;
+use common::test_dir::*;
+
+//------------------------------------------
+
+const USAGE: &str = "Usage: era_dump [options] {device|file}\n\
+                     Options:\n  \
+                       {-h|--help}\n  \
+                       {-o <xml file>}\n  \
+                       {-V|--version}\n  \
+                       {--repair}\n  \
+                       {--logical}";
+
+//------------------------------------------
+
+struct EraDump;
+
+impl<'a> Program<'a> for EraDump {
+    fn name() -> &'a str {
+        "cache_dump"
+    }
+
+    fn path() -> &'a std::ffi::OsStr {
+        ERA_DUMP.as_ref()
+    }
+
+    fn usage() -> &'a str {
+        USAGE
+    }
+
+    fn arg_type() -> ArgType {
+        ArgType::InputArg
+    }
+
+    fn bad_option_hint(option: &str) -> String {
+        msg::bad_option_hint(option)
+    }
+}
+
+impl<'a> InputProgram<'a> for EraDump {
+    fn mk_valid_input(td: &mut TestDir) -> Result<std::path::PathBuf> {
+        mk_valid_md(td)
+    }
+
+    fn file_not_found() -> &'a str {
+        msg::FILE_NOT_FOUND
+    }
+
+    fn missing_input_arg() -> &'a str {
+        msg::MISSING_INPUT_ARG
+    }
+
+    fn corrupted_input() -> &'a str {
+        msg::BAD_SUPERBLOCK
+    }
+}
+
+impl<'a> MetadataReader<'a> for EraDump {}
+
+//------------------------------------------
+
+test_accepts_help!(EraDump);
+test_accepts_version!(EraDump);
+test_rejects_bad_option!(EraDump);
+
+test_missing_input_arg!(EraDump);
+test_input_file_not_found!(EraDump);
+test_input_cannot_be_a_directory!(EraDump);
+test_unreadable_input_file!(EraDump);
+test_tiny_input_file!(EraDump);
+
+//------------------------------------------
+
+// TODO: share with thin_dump
+#[test]
+fn dump_restore_cycle() -> Result<()> {
+    let mut td = TestDir::new()?;
+    let md = mk_valid_md(&mut td)?;
+    let output = run_ok_raw(ERA_DUMP, args![&md])?;
+
+    let xml = td.mk_path("meta.xml");
+    let mut file = OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(true)
+        .open(&xml)?;
+    file.write_all(&output.stdout[0..])?;
+    drop(file);
+
+    let md2 = mk_zeroed_md(&mut td)?;
+    run_ok(ERA_RESTORE, args!["-i", &xml, "-o", &md2])?;
+
+    let output2 = run_ok_raw(ERA_DUMP, args![&md2])?;
+    assert_eq!(output.stdout, output2.stdout);
+
+    Ok(())
+}
