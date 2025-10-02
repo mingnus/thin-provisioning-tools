57a70d141f07f59f5b2515b5078ce001b46fefa0
Ming-Hung Tsai <mtsai@redhat.com>
2025-10-02 14:37:47 +0800

    [thin_check] Log additional memory usage info

--- src/pdata/space_map/aggregator.rs
+++ src/pdata/space_map/aggregator.rs
@@ -654,6 +654,10 @@ impl Aggregator {
         total
     }
 
+    pub fn get_nr_regions(&self) -> usize {
+        self.regions.len()
+    }
+
     pub fn inc_allocated(&self, count: u64) {
         let mut nr_allocated = self.nr_allocated.lock().unwrap();
         *nr_allocated = nr_allocated.saturating_add(count);
